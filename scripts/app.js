1cb33dc783172a5b41f59cb9418d80ed06f75ab2
Yuval Allweil <themarquisdesheric@gmail.com>
2017-03-07 22:33:24 -0800

    Seeing what I can do here...

--- scripts/app.js
+++ scripts/app.js
@@ -5,6 +5,34 @@ var projects = [];
 function Project(app) {
   this.title = app.title;
   this.url = app.url;
+  this.hosted = app.hosted;
   this.image = app.image;
   this.body = app.body;
 }
+
+Project.prototype.toHtml = function() {
+  var $newProject = $('article.template').clone();
+  //set title
+  $newProject.find('h2 a').text(this.title);
+  //set GitHub URL
+  $newProject.find('a[class="github-link"]').attr('href', this.url);
+  //set image
+  $newProject.find('img').attr('src', this.image);
+  //set body
+  $newProject.find('section.description').html(this.body);
+  //set hosted URL
+  $newProject.find('a[class="hosted-link"]').attr('href', this.hosted);
+  //remove template class
+  $newProject.removeClass('template');
+
+  return $newProject;
+};
+
+githubProjects.forEach(function(proj) {
+  //create project instances and push them into projects array
+  projects.push(new Project(proj));
+});
+
+projects.forEach(function(post) {
+  $('#projects').append(post.toHtml());
+});
