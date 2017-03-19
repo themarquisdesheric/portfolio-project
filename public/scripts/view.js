f1bfbb29daf522ace7ce0d533cb0bc324bce5bd1
Yuval Allweil <themarquisdesheric@gmail.com>
2017-03-19 11:16:25 -0700

    Refactored code into arrow function syntax; wrapped model and view in IIFEs

--- public/scripts/view.js
+++ public/scripts/view.js
@@ -1,28 +1,30 @@
 'use strict';
 
-//wrap in an IIFE
+(function(module) {
+  var view = {};
 
-var view = {};
+  view.tabbedContent = function() {
+    $('nav').on('click', '.tab', function(event) {
+      var clicked = $(this).attr('data-content');
+      event.preventDefault();
+      //bold active tab
+      $('.tab').removeClass('active');
+      $(this).addClass('active');
+      //hide all sections and display relevant section
+      $('section').not('#contact, .description').hide();
+      $(`#${clicked}`).fadeIn(800);
 
-view.tabbedContent = function() {
-  $('nav').on('click', '.tab', function(event) {
-    var clicked = $(this).attr('data-content');
-    event.preventDefault();
-    //bold active tab
-    $('.tab').removeClass('active');
-    $(this).addClass('active');
-    //hide all sections and display relevant section
-    $('section').not('#contact, .description').hide();
-    $(`#${clicked}`).fadeIn(800);
+      if (clicked === 'projects') {
+        $('hr').hide();
+      } else {
+        $('hr').show();
+      }
+    });
 
-    if (clicked === 'projects') {
-      $('hr').hide();
-    } else {
-      $('hr').show();
-    }
-  });
+    $('#projects').hide();
+  };
 
-  $('#projects').hide();
-};
+  module.view = view;
+  view.tabbedContent();
 
-view.tabbedContent();
+})(window);
