a4492bff5e61456e0f418efe0e02b2a2a13a3530
Yuval Allweil <themarquisdesheric@gmail.com>
2017-03-19 10:11:36 -0700

    server.js now using arrow functions

--- server.js
+++ server.js
@@ -7,16 +7,8 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static('./public'));
 
-//use arrow functions here
+app.get('/', (request, response) => response.sendFile('public/index.html', {root: '.'}));
 
-app.get('/', function(request, response) {
-  response.sendFile('public/index.html', {root: '.'});
-});
+app.get('*', (request, response) => response.status(404).send('404 error: page not found'));
 
-app.get('*', function(request, response) {
-  response.status(404).send('404 error: page not found');
-});
-
-app.listen(PORT, function() {
-  console.log(`Running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Running on port ${PORT}`));
