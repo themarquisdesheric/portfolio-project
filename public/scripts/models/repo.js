567c92c764e335e311ff6eac38e360d592e9e543
Yuval Allweil <themarquisdesheric@gmail.com>
2017-03-22 16:02:43 -0700

    Working out bugs

--- public/scripts/models/repo.js
+++ public/scripts/models/repo.js
@@ -7,19 +7,22 @@
 
   repos.requestRepos = function(callback) {
     $.get('https://api.github.com/users/themarquisdesheric/repos')
-      .then(res => {
-        repos.all = res
-        //return all repos that were not forked, and match the projects I want
-        .filter(r => !r.fork && (r.name === 'busMall' || r.name === 'cookie-stand' || r.name === 'project-site'))
-        .map(repo => {
-          return {
-            url: repo.svn_url,
-            description: repo.description,
-            name: repo.name
-          }
-        });
-        callback();
-      }).catch(err => console.error);
+      .done(function(data) {
+        repos.all = data;
+      }).done(callback);
+      // .then(res => {
+      //   repos.all = res
+      //   //return all repos that were not forked, and match the projects I want
+      //   .filter(r => !r.fork && (r.name === 'busMall' || r.name === 'cookie-stand' || r.name === 'project-site'))
+      //   .map(repo => {
+      //     return {
+      //       url: repo.svn_url,
+      //       description: repo.description,
+      //       name: repo.name
+      //     }
+      //   });
+      //   callback();
+      // }).catch(err => console.error);
   }
 
   repos.with = attr => repos.all.filter(repo => repo[attr]);
